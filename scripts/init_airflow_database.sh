a368eb064602b10fd267a98f2ff8daf071444ea2
Terry Lee <uwgdw88@gmail.com>
2024-05-16 10:42:16 -0400

    configurations for local Airflow and postgres

---
+++ scripts/init_airflow_database.sh
@@ -0,0 +1,11 @@
+cd ..
+
+docker-compose run airflow-webserver airflow db init
+
+docker-compose run airflow-webserver airflow users create \
+    --username admin \
+    --password admin \
+    --firstname Admin \
+    --lastname User \
+    --role Admin \
+    --email admin@example.com
